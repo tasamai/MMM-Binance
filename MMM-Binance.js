616672d5378c5d7cd998be259cffe111f17df118
tasamai <samaithomas@gmail.com>
2021-03-13 21:35:24 +0000

    test functions

---
+++ MMM-Binance.js
@@ -0,0 +1,27 @@
+Module.register("mmm-test", {
+    defaults: {},
+    start: function () {},
+    getDom: function() {},
+    notificationReceived: function() {},
+    socketNotificationReceived: function() {},
+
+	start: function (){
+		this.count = 0
+		var timer = setInterval(()=>{
+		  this.updateDom()
+		  this.count++
+		}, 1000)
+	  },
+
+    getDom: function() {
+        var element = document.createElement("div")
+        element.className = "myContent"
+        element.innerHTML = "Hello, Tom!"
+		var subElement = document.createElement("p")
+ 		subElement.innerHTML = "Count:" + this.count
+		subElement.id = "COUNT"
+		element.appendChild(subElement)
+		return element
+      },
+
+ })
